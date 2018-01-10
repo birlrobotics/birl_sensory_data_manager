7834e72dab2d977ec7236cc0c4755719505132d5
sklaw <sk.law.lsq@gmail.com>
2018-01-10 18:26:18 +0800

    change pkg names

---
+++ src/birl_offline_data_handler/rosbag_handler/interface.py
@@ -0,0 +1,45 @@
+import core
+import glob, os
+
+
+class InvalidRosbagPath(Exception): pass
+
+
+def convert_rosbag_to_csv(path_to_rosbag):
+    """Convert ROS bag file(s) to csv file(s).
+
+    If the ROS bag contains more than one ROS topics, 
+    for each topic, one csv will be output. The 
+    output csv(s) will be stored in a folder of the 
+    same name and in the same folder as the original 
+    ROS bag. 
+
+    Args:
+        path_to_rosbag: A path to a ROS bag file or 
+            a folder of ROS bag files. In the latter 
+            case, all ROS bag files in that folder
+            will be processed. 
+
+    Returns:
+        None
+
+    Raises:
+        We don't intentionally raise any exception. If you catch some, it may be thrown by `rosbag`_.
+
+    .. _rosbag:
+        http://docs.ros.org/api/rosbag/html/python/
+
+    """
+
+    if os.path.isdir(path_to_rosbag):
+        list_of_bag = glob.glob(
+            os.path.join(
+                path_to_rosbag,
+                "*.bag",
+            )
+        )
+    elif os.path.isfile(path_to_rosbag):
+        list_of_bag = [path_to_rosbag]
+    else:
+        raise InvalidRosbagPath()
+    

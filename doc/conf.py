b79708b37a9e6569be0f66c0c9b089ed6aa56555
sklaw <none>
2018-01-17 13:29:08 +0800

    ignore catkin if import fails

--- doc/conf.py
+++ doc/conf.py
@@ -20,12 +20,15 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-import os
-import catkin_pkg.package
-catkin_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) 
-catkin_package = catkin_pkg.package.parse_package(os.path.join(catkin_dir, catkin_pkg.package.PACKAGE_MANIFEST_FILENAME))
-version = catkin_package.version
-release = catkin_package.version
+try:
+    import os
+    import catkin_pkg.package
+    catkin_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) 
+    catkin_package = catkin_pkg.package.parse_package(os.path.join(catkin_dir, catkin_pkg.package.PACKAGE_MANIFEST_FILENAME))
+    version = catkin_package.version
+    release = catkin_package.version
+except:
+    pass
 
 
 # -- General configuration ------------------------------------------------

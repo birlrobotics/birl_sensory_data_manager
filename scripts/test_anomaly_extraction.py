2a2977129ac24f226bc82d1579408ff3bd7a720e
sklaw <none>
2018-01-19 00:21:36 +0800

    new test scritps that will check return type

--- scripts/test_anomaly_extraction.py
+++ scripts/test_anomaly_extraction.py
@@ -7,7 +7,8 @@ from birl_offline_data_handler_test.util import (
     delete_test_dir,
     same_dir_test,
 )    
-
+import traceback
+import pandas
 import logging 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
@@ -15,22 +16,37 @@ logger = logging.getLogger()
 if __name__ == '__main__':
     score_dir = get_score_dir("good_anomaly_extraction")
 
-    logger.info("Test anomaly extract for a folder of rosbags")
     test_dir = create_a_test_dir()
     clean_test_flag = True
     rae = RosbagAnomalyExtractor(test_dir)
-    rae.get_anomaly_csv(
+    ret = rae.get_anomaly_csv(
         "/tag_multimodal",
         "/anomaly_detection_signal",
         4,
         10,
     )
     try:
+        logger.info("Test anomaly extraction for a folder of rosbags")
         assert same_dir_test(test_dir, score_dir)
     except AssertionError as e:
         logger.error('failed. test_folder(\"%s\") and score_folder(\"%s\") are different.'%(test_dir, score_dir))
         clean_test_flag = False
     else:
         logger.info("passed.")
+    
+    try:
+        logger.info("Test return value types.")
+        assert len(ret)==2
+        assert type(ret[0][0])==str
+        assert type(ret[0][1])==list
+        assert type(ret[0][1][0][0])==str
+        assert type(ret[0][1][0][1])==pandas.core.frame.DataFrame
+    except AssertionError as e:
+        traceback.print_exc()
+        logger.error('failed.')
+        clean_test_flag = False
+    else:
+        logger.info("passed.")
+
     if clean_test_flag:
         delete_test_dir(test_dir)
--- scripts/test_rosbag_handler.py
+++ scripts/test_rosbag_handler.py
@@ -10,7 +10,8 @@ from birl_offline_data_handler_test.util import (
     delete_test_dir,
     same_dir_test,
 )    
-
+import traceback
+import pandas
 import glob, os
 import logging 
 logging.basicConfig(level=logging.INFO)
@@ -41,16 +42,27 @@ if __name__ == '__main__':
     else:
         logger.error("failed.")
 
-    logger.info("Test converting rosbag folder.")
-    o.get_csv_of_a_topic("/tag_multimodal")
-    o.get_csv_of_a_topic("/anomaly_detection_signal")
+    ret = o.get_csv_of_a_topic("/tag_multimodal")
+    ret = o.get_csv_of_a_topic("/anomaly_detection_signal")
     try:
+        logger.info("Test converting rosbag folder.")
         assert same_dir_test(test_dir, score_dir)
     except AssertionError as e:
         logger.error('failed. test_folder(\"%s\") and score_folder(\"%s\") are different.'%(test_dir, score_dir))
+    else:
+        logger.info("passed.")
+
+    try:
+        logger.info("Test return value types.")
+        assert len(ret)==2
+        assert type(ret[0][0])==str
+        assert type(ret[0][1])==pandas.core.frame.DataFrame
+    except AssertionError as e:
+        logger.error('failed.')
         clean_test_flag = False
     else:
         logger.info("passed.")
+
     if clean_test_flag:
         delete_test_dir(test_dir)
         
@@ -66,8 +78,8 @@ if __name__ == '__main__':
     )
     for bag_file in list_of_bag_file:
         o = RosbagHandler(bag_file)
-        o.get_csv_of_a_topic("/tag_multimodal")
-        o.get_csv_of_a_topic("/anomaly_detection_signal")
+        ret = o.get_csv_of_a_topic("/tag_multimodal")
+        ret = o.get_csv_of_a_topic("/anomaly_detection_signal")
         fname = os.path.basename(bag_file)[:-4]     # strip .bag
 
         test_bag_dir = os.path.join(test_dir, fname)
@@ -77,12 +89,22 @@ if __name__ == '__main__':
         try:
             assert same_dir_test(test_bag_dir, score_bag_dir)
         except AssertionError as e:
-            logger.error('failed. test_bag_folder(\"%s\") and score_bag_folder(\"%s\") are different.'%(test_bag_dir, score_bag_dir))
-            clean_test_flag = False
-        except OSError as e:
-            logger.error('failed. %s'%(e,))
+            logger.error('failed. test_folder(\"%s\") and score_folder(\"%s\") are different.'%(test_dir, score_dir))
             clean_test_flag = False
         else:
             logger.info("passed.")
+
+        try:
+            logger.info("Test return value types.")
+            assert len(ret)==1
+            assert ret[0][0]==bag_file
+            assert type(ret[0][1])==pandas.core.frame.DataFrame
+        except AssertionError as e:
+            traceback.print_exc()
+            logger.error('failed.')
+            clean_test_flag = False
+        else:
+            logger.info("passed.")
+
     if clean_test_flag:
         delete_test_dir(test_dir)
